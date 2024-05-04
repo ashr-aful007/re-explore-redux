eb38108329d940064173960e9309a4d1701c4609
ashr-aful007 <ashraful99980@gmail.com>
2024-05-04 08:32:41 +0600

    add redux config store

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ function App() {
 
 
   return (
-    <>
-      <h1>Re explore redux</h1>
-    </>
+    <div className="h-screen flex w-full justify-center items-center">
+      <div className="flex border p-10 border-purple-300 rounded-md bg-gray-300">
+        <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">increment</button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white">DeCrement</button>
+      </div>
+    </div>
   )
 }
 
